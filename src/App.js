7144aa4c7f065eacda9c1ed402ec8e954c3f2fa7
Babelek <naati.czop@gmail.com>
2019-04-24 01:45:36 +0200

    create  appHeader, contactList, contactItem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  function App() {
+    return (
+      <div>
+        <AppHeader/>
+        <main className="ui main text container">
+          <ContactsList />
+        </main>
+      </div>
+    );
+  }
+  
+  function AppHeader () {
+    return (    
+      <header className="ui fixed menu">  
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"></link>  
+        <nav class="ui container">
+          <a href="#" class="header item">
+            <img class="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
+            Lista kontaktów
+          </a>
+          <div class="header item">
+            <button class="ui button">Dodaj</button>
+          </div>
+        </nav>
       </header>
-    </div>
-  );
-}
+    );
+  }
+  
+  function ContactsList() {
+    return (
+      <ul className="ui relaxed divided list selection">
+        <ContactItem
+          login="typeofweb1"
+          name="Lena"
+          department="JavaScript Developer"
+        />
+        <ContactItem
+          login="typeofweb2"
+          name="Brian"
+          department="Human Resources"
+        />
+        <ContactItem
+          login="typeofweb3"
+          name="Rick"
+          department="QA"
+        />
+      </ul>
+    );
+  }
+  
+  function ContactItem({ login, name, department }) {
+    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+    return (
+      <li className="item">
+        <img src={imgUrl} className="ui mini rounded image" />
+        <div className="content">
+          <h4 className="header">{name}</h4>
+          <div className="description">{department}</div>
+        </div>
+      </li>
+    );
+  }   
 
 export default App;
