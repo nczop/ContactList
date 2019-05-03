b69d0e48814f2c3f13e338e42c473cea396b083c
Babelek <naati.czop@gmail.com>
2019-05-03 16:28:46 +0200

    Updates

---
+++ precache-manifest.98f117524a7544acc9f119af2cd44671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68512bf9d3a728edf9ab94c8a4a33c31",
+    "url": "/ContactList/index.html"
+  },
+  {
+    "revision": "901535af6fdc9c7795ff",
+    "url": "/ContactList/static/css/main.f93d1d02.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ContactList/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "901535af6fdc9c7795ff",
+    "url": "/ContactList/static/js/main.b4e81140.chunk.js"
+  },
+  {
+    "revision": "6233cebcc55f13ab8712",
+    "url": "/ContactList/static/js/runtime~main.d226de48.js"
+  }
+]);
